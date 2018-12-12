c0912b7f62efb575d3c144e892a6e389a9f0ef8a
novakvova <novakvova@gmail.com>
2018-12-12 09:20:47 +0200

    Begin site

---
+++ _footer.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <?php include_once "_footer_container.php"; ?>
+</div>
+
+
+<script href="js/jquery-3.3.1.min.js"></script>
+<script href="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,56 @@
+<?php include_once "_header.php"; ?>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title col-12 col-lg-8">
+                <h2 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2"><strong>
+                        CONTACT FORM TEMPLATE</strong></h2>
+                <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-5">
+                    Easily add subscribe and contact forms without any server-side integration.
+                </h3>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="media-container-column col-lg-8" data-form-type="formoid">
+                <div data-form-alert="" hidden="">
+                    Thanks for filling out the form!
+                </div>
+
+                <form class="mbr-form" action="https://mobirise.com/" method="post" data-form-title="Mobirise Form"><input type="hidden" data-form-email="true" value="zdGVBcNRThcdWUUfI+3PsLnnLtHc8JIdEhub3sFJZu3mJPkE6oxv5FmU79KVvaLjKSmEfsOBbKYj1Cps8hm2o1hmKtkhiOdHe4oDrPZiHdR0Yvomv7W9ZOtj6J4piMeE">
+                    <div class="row row-sm-offset">
+                        <div class="col-md-4 multi-horizontal" data-for="name">
+                            <div class="form-group">
+                                <label class="form-control-label mbr-fonts-style display-7" for="name-form1-2w">Name</label>
+                                <input type="text" class="form-control" name="name" data-form-field="Name" required="" id="name-form1-2w">
+                            </div>
+                        </div>
+                        <div class="col-md-4 multi-horizontal" data-for="email">
+                            <div class="form-group">
+                                <label class="form-control-label mbr-fonts-style display-7" for="email-form1-2w">Email</label>
+                                <input type="email" class="form-control" name="email" data-form-field="Email" required="" id="email-form1-2w">
+                            </div>
+                        </div>
+                        <div class="col-md-4 multi-horizontal" data-for="phone">
+                            <div class="form-group">
+                                <label class="form-control-label mbr-fonts-style display-7" for="phone-form1-2w">Phone</label>
+                                <input type="tel" class="form-control" name="phone" data-form-field="Phone" id="phone-form1-2w">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group" data-for="message">
+                        <label class="form-control-label mbr-fonts-style display-7" for="message-form1-2w">Message</label>
+                        <textarea type="text" class="form-control" name="message" rows="7" data-form-field="Message" id="message-form1-2w"></textarea>
+                    </div>
+
+                        <span class="input-group-btn">
+                            <button href="" type="submit" class="btn btn-primary btn-form display-4">SEND FORM</button>
+                        </span>
+                </form>
+            </div>
+        </div>
+    </div>
+
+<?php include_once "_footer.php"; ?>
